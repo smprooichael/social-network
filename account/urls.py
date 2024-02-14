955e989c9dbbc0b972f4672bac5a1c5737634a10
Meysam <smprooichael@gmail.com>
2024-02-14 21:00:46 +0330

    complete register, login and logout

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import UserRegestration
+from .views import UserRegestration, UserLogin, UserLogout
 
+app_name = 'account'
 urlpatterns = [
     path('register/', UserRegestration.as_view(), name='user_register'),
+    path('login/', UserLogin.as_view(), name='user_login'),
+    path('logout/', UserLogout.as_view(), name='user_logout'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserLoginForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 class UserRegestration(View):
+    form_class = UserRegisterForm
+    template_name = 'account/register.html'
     def get(self, request):
-        form = UserRegisterForm()
-        return render(request, 'account/register.html', {'form': form})
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(username=cd['username'], email=cd['email'], password=cd['password1'])
+            messages.success(request, 'You registered successfully!', 'success')
+            return redirect('home:home')
+        return render(request, self.template_name, {'form': form})
+
+
+class UserLogin(View):
+    form_class = UserLoginForm
+    template_name = 'account/login.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You logged in', 'success')
+                return redirect('home:home')
+            messages.error(request, 'Username or password is wrong', 'warning')
+        return render(request, self.template_name, {'form':form})
+
+class UserLogout(View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, 'You logged out successfully', 'success')
+        return redirect('home:home')
