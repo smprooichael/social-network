a656a13b20999bfa0b239a833679e80ecde4bb62
meysam <smprooichael@gmail.com>
2024-03-07 08:26:00 +0330

    show replies and reply form

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('post/detail/<int:post_id>/<slug:post_slug>', views.PostDetailView.as_view(), name='post_detail'),
     path('post/delete/<int:post_id>/', views.PostDeleteView.as_view(), name='post_delete'),
     path('post/update/<int:post_id>/', views.PostUpdateView.as_view(), name='post_update'),
-    path('post/create/', views.PostCreateView.as_view(), name='post_create')
+    path('post/create/', views.PostCreateView.as_view(), name='post_create'),
+    path('reply/<int:post_id>/<int:comment_id>/', views.ReplyCommentView.as_view(), name='reply_comment')
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from .models import Post
+from .models import Post, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from .forms import PostCreateUpdateForm
+from .forms import PostCreateUpdateForm, CommentCreateForm, CommentReplyForm
 from django.utils.text import slugify
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 class HomeView(View):
     def get(self, request):
@@ -13,10 +15,26 @@ class HomeView(View):
 
 
 class PostDetailView(View):
-    def get(self, request, post_id, post_slug):
-        post = get_object_or_404(Post, pk=post_id, slug=post_slug)
-        Comments = post.pcomments.filter(is_reply=False)
-        return render(request, 'home/detail.html', {'post':post, 'Comments':Comments})
+    form_class = CommentCreateForm
+    form_class_reply = CommentReplyForm
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'], slug=kwargs['post_slug'])
+        return super().setup(request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        Comments = self.post_instance.pcomments.filter(is_reply=False)
+        return render(request, 'home/detail.html', {'post':self.post_instance, 'Comments':Comments, 'form': self.form_class, 'form_reply': self.form_class_reply})
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = self.post_instance
+            new_comment.save()
+            messages.success(request, 'Your comment submitted successfully!', 'success')
+            return redirect('home:post_detail', self.post_instance.id, self.post_instance.slug)
+
 
 
 class PostDeleteView(LoginRequiredMixin, View):
@@ -62,6 +80,7 @@ class PostUpdateView(LoginRequiredMixin, View):
 class PostCreateView(LoginRequiredMixin, View):
     form_class = PostCreateUpdateForm
     template_name = 'home/create.html'
+
     def get(self, request, *args, **kwargs):
         form = self.form_class()
         return render(request, self.template_name, {'form':form})
@@ -75,3 +94,22 @@ class PostCreateView(LoginRequiredMixin, View):
             new_post.save()
             messages.success(request, 'The post created successfully', 'success')
             return redirect('home:post_detail', new_post.id, new_post.slug)
+
+
+class ReplyCommentView(LoginRequiredMixin, View):
+    form_class = CommentReplyForm
+
+    def post(self, request, post_id, comment_id):
+        post = get_object_or_404(Post, pk=post_id)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = post
+            new_comment.reply = comment
+            new_comment.is_reply = True
+            new_comment.save()
+            messages.success(request, 'Your reply submitted successfully.', 'success')
+        return redirect('home:post_detail', post.id, post.slug)
+
