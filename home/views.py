4ff52e7f9d4209a66cdc8ddd5a436bac18fca3b1
meysam <smprooichael@gmail.com>
2024-03-07 10:57:55 +0330

    search field remove get

--- home/views.py
+++ home/views.py
@@ -15,7 +15,7 @@ class HomeView(View):
     def get(self, request):
         posts = Post.objects.all()
         if request.GET.get('search'):
-            posts = posts.filter(body__icontains=request.GET.get('search'))
+            posts = posts.filter(body__icontains=request.GET['search'])
         return render(request, 'home/index.html', {'posts': posts, 'form': self.form_class})
 
 
